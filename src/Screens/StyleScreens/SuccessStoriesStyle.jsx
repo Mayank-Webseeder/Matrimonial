0037352287fc870b5ec7ca0a51df7aa638ab3363
PallaviPatel782 <patel.pallavi@webseeder.in>
2025-04-24 16:56:14 +0530

    updated

--- src/Screens/StyleScreens/SuccessStoriesStyle.jsx
+++ src/Screens/StyleScreens/SuccessStoriesStyle.jsx
@@ -25,6 +25,7 @@ const styles = StyleSheet.create({
         elevation: 3,
         shadowColor: '#000',
         shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 2 },
         shadowRadius: 4,
       },
       collabHeader: {
